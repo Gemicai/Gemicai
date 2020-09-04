0d3e95ec3f866d9ae2afd277c6992567f01180e8
mzjaworski <m.jaworski97@wp.pl>
2020-09-04 16:31:18 +0200

    renamed some scripts andadded a function that returns {tensor, label} tuple

--- dicom_utilities.py
+++ dicom_utilities.py
@@ -2,11 +2,15 @@ import gzip
 import pydicom as dicom
 import os
 import pandas as pd
-from datetime import datetime
 from matplotlib import pyplot as plt
 import sys
 import string
 import pathlib
+from PIL import Image
+import glob
+import torchvision
+import torch
+import numpy
 
 # if needed we can export it later
 fields_of_interest = ['Rows', 'StudyDate', 'SeriesTime', 'ContentTime', 'StudyInstanceUID', 'SeriesInstanceUID',
@@ -26,38 +30,39 @@ def load_dicom(filename):
         ds.file_meta.TransferSyntaxUID = dicom.uid.ImplicitVRLittleEndian
     except:
         print("Please priovide a valid .dcm or .dcm.gz file")
-        sys.exit(1)
+        raise
     return ds
 
 
-def get_directory_path(path):
-    index = path.rfind('/')
-    return os.path.normpath(path[0:index])
+def get_os_directory_path(path):
+    normalized_path = str(pathlib.Path(path))
+    index = normalized_path.rfind('/')
+    return os.path.normpath(normalized_path[0:index])
 
 
 def create_dir_if_does_not_exist(path):
-    dir_name = get_directory_path(path)
+    dir_name = get_os_directory_path(path)
     if not os.path.exists(dir_name):
         os.makedirs(dir_name)
 
 
-def open_directory(path, dir_type):
+def open_directory(path):
     try:
         return pathlib.Path(path)
     except:
-        print("Could not open a specified " + dir_type + " directory")
-        sys.exit(1)
+        print("Could not open directory: " + str(path))
+        raise
 
 
 def get_file_name(path):
     try:
-        return str(path).split('.')[0].split('\\')[-1]
+        return str(pathlib.Path(path)).split('.')[0].split('/')[-1]
     except:
         print("Cannot parse a file path: " + str(path))
-        sys.exit(1)
+        raise
 
 
-def extract_png(input, output, pickle):
+def dicom_to_png_pkl(input, output, pickle):
     ds = load_dicom(os.path.normpath(input))
     # try to fetch the relevant data (so maybe we can process it later)
     dcm_data = []
@@ -84,7 +89,7 @@ def extract_png(input, output, pickle):
         plt.imsave(os.path.normpath(output), image, cmap="gray")
     except:
         print("Invalid image path")
-        sys.exit(1)
+        raise
 
     # create data frame to keep records of the images
     df = pd.DataFrame(dcm_data, fields_of_interest)
@@ -94,34 +99,48 @@ def extract_png(input, output, pickle):
         df.to_pickle(os.path.normpath(pickle))
     except:
         print("Invalid pickle location")
-        sys.exit(1)
+        raise
 
 
-def extract_pngs(input_folder, output_folder):
+# Process dicom files in the input_folder and stores result in the output_fodler
+def process_dicom_from_to_folder(input_folder, output_folder):
     # Open specified input directory
-    in_dir = open_directory(input_folder, "input");
+    in_dir = open_directory(input_folder);
     # Process files in the input directory
     for path in in_dir.iterdir():
         if path.is_file():
             file_name = get_file_name(path)
             image_output = output_folder + '/images/' + file_name + '.png'
             pickle_output = output_folder + '/pickle/' + file_name + '.pkl'
-            extract_png(path, image_output, pickle_output)
+            extract_dicom_data(path, image_output, pickle_output)
 
 
 # Returns ({imgage as tensor}, {label})
-def extract_img_and_label(dicom_file_path):
-    df = load_dicom(dicom_file_path)
-    # For the neural network, the image needs to be represented as a tensor
-    # TODO: implement this
-    tensor = dicom_file_path
-    label = getattr(df, 'BodyPartExamined')
+def dicom_extract_img_and_label(dicom_file_path):
+    # try to load a dicom file
+    ds = load_dicom(dicom_file_path)
+
+    # transform pixel_array into a format accepted by the pytorch
+    norm = plt.Normalize(vmin=ds.pixel_array.min(), vmax=ds.pixel_array.max())
+    data = torch.from_numpy(norm(ds.pixel_array).astype(numpy.float32))
+
+    # if we want to print the resulting image remove the last transform and call tensor.show() after create_tensor
+    create_tensor = torchvision.transforms.Compose([
+        torchvision.transforms.ToPILImage(),
+        torchvision.transforms.Grayscale(3),
+        torchvision.transforms.Resize((244, 244)),
+        torchvision.transforms.ToTensor()
+    ])
+
+    tensor = create_tensor(data)
+    label = getattr(ds, 'BodyPartExamined')
     return tensor, label
 
 
-extract_pngs('examples', 'examples/pngs')
-print(extract_img_and_label('examples/1.dcm.gz'))
-print(extract_img_and_label('examples/2.dcm.gz'))
-print(extract_img_and_label('examples/3.dcm.gz'))
-print(extract_img_and_label('examples/4.dcm.gz'))
-print(extract_img_and_label('examples/5.dcm.gz'))
+# process_dicom_from_to_folder('examples', 'examples/pngs')
+print(dicom_extract_img_and_label("examples/1.dcm.gz"))
+#print(extract_img_and_label('examples/1.dcm.gz'))
+#print(extract_img_and_label('examples/2.dcm.gz'))
+#print(extract_img_and_label('examples/3.dcm.gz'))
+#print(extract_img_and_label('examples/4.dcm.gz'))
+#print(extract_img_and_label('examples/5.dcm.gz'))
