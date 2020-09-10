40b8149acad6a26a2f837bb74b4e24c079913e50
Niek <n.l.heinen@student.utwente.nl>
2020-09-10 15:20:11 +0200

    Should be ready to extract only usefull dicom data now
    Former-commit-id: 5824f2724531362357c94dd564d3034e5aad5350

--- create_dataset.py
+++ create_dataset.py
@@ -5,7 +5,7 @@ import pickle
 
 filename = ("dicom_objects_%08i.pkl" % i for i in count(1))
 
-data_origin = '/mnt/data2/pukkaj/teach/study/PJ_TEACH/PJ_STUDY/'
+data_origin = '/mnt/data2/pukkaj/teach/study/PJ_TEACH/PJ_RESEARCH/'
 data_destination = '/home/nheinen/gemicai/dicom_objects/dx'
 os.chdir(data_origin)
 
@@ -16,9 +16,13 @@ for root, dirs, files in os.walk('.'):
     for file in files:
         try:
             dicom = du.Dicom(file)
+            if dicom.modality in modalities:
+                temp_list.append(dicom)
+                if len(temp_list) >= 1000:
+                    with open(next(filename), 'wb') as output:
+                        pickle.dump(temp_list, output, pickle.HIGHEST_PROTOCOL)
+                        temp_list = []
         except:
             print('Exception loading {} in folder {}'.format(file, root))
-        if dicom.modality == 'DX':
-            if len(temp_list) >= 1000:
-                with open(next(filename), 'wb') as output:
-                    pickle.dump(temp_list, output, pickle.HIGHEST_PROTOCOL)
+
+
