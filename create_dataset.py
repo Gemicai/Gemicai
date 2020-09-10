72fea14a677577ffc59c0c71a6c6dcf95deef470
Niek <n.l.heinen@student.utwente.nl>
2020-09-10 15:59:19 +0200

    Added exception handeling in create_dataset.py
    Former-commit-id: 55f7a73ca74fe5c3c428705741583f1813a01df1

--- create_dataset.py
+++ create_dataset.py
@@ -1,28 +1,34 @@
-import dicom_utilities as du
+
 import os
 from itertools import count
 import pickle
+import dicom
 
 filename = ("dicom_objects_%08i.pkl" % i for i in count(1))
 
 data_origin = '/mnt/data2/pukkaj/teach/study/PJ_TEACH/PJ_RESEARCH/'
-data_destination = '/home/nheinen/gemicai/dicom_objects/dx'
+data_destination = '/home/nheinen/gemicai/dicom_objects/'
 os.chdir(data_origin)
 
 modalities = ['CT', 'MR', 'DX', 'MG', 'US', 'PT']
 temp_list = []
+attribute_errors = 0
 
 for root, dirs, files in os.walk('.'):
     for file in files:
         try:
-            dicom = du.Dicom(file)
-            if dicom.modality in modalities:
-                temp_list.append(dicom)
+            d = dicom.Dicom(root+'/'+file)
+            if d.modality in modalities:
+                temp_list.append(d)
                 if len(temp_list) >= 1000:
-                    with open(next(filename), 'wb') as output:
+                    with open(data_destination+next(filename), 'wb') as output:
                         pickle.dump(temp_list, output, pickle.HIGHEST_PROTOCOL)
                         temp_list = []
-        except:
-            print('Exception loading {} in folder {}'.format(file, root))
+        except AttributeError:
+            attribute_errors += 1
+        except Exception as ex:
+            template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+            message = template.format(type(ex).__name__, ex.args)
+            print(message)
 
-
+print('Finished ! Total attribute errors: {}'.format(attribute_errors))
