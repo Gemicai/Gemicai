99908285d9d8573ef8aabff79270102d2dbdba80
Niek <n.l.heinen@student.utwente.nl>
2020-09-17 10:46:49 +0200

    test

--- misc/get_labels.py
+++ misc/get_labels.py
@@ -8,26 +8,28 @@ import PickleDataSet
 import classifier
 
 data_origin = '/home/nheinen/gemicai/dicom_objects/DX/000001.dicomos.gz'
-# data_origin = '../examples/compressed/DX/000001.dicomos.gz'
+data_origin = '../examples/compressed/DX/000001.dicomos.gz'
 
 
-pds = PickleDataSet.PickleDataSet(data_origin, ['tensor', 'bpe'])
-pds_iter = iter(pds)
-cnt = Counter()
-for _ in range(1000):
-    label = next(pds_iter)[1]
-    cnt.update(label)
-
-for value, count in cnt.most_common():
-    print(value, count)
-
-
-# This should work but doesn't
-# dataloader = classifier.get_data_loader(data_directory=data_origin)
+# pds = PickleDataSet.PickleDataSet(data_origin, ['tensor', 'bpe'])
+# pds_iter = iter(pds)
+# cnt = Counter()
+# s = {}
+# for _ in range(1000):
+#     label = next(pds_iter)[1]
+#     cnt.update(label)
+#     s.update(s)
 #
-# for i, data in enumerate(dataloader):
-#     # get the inputs; data is a list of [inputs, labels]
-#     tensor, labels = data
-#     print(data)
+# for value, count in cnt.most_common():
+#     print(value, count)
+#
+#
+# # This should work but doesn't
+dataloader = classifier.get_data_loader(data_directory=data_origin)
+
+for i, data in enumerate(dataloader):
+    # get the inputs; data is a list of [inputs, labels]
+    tensor, labels = data
+    print(data)
 
 
