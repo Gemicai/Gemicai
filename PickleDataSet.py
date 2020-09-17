b1051ac63b2cc6495f1af7b5a1a67d4eaad0021a
mzjaworski <m.jaworski97@wp.pl>
2020-09-17 12:06:14 +0200

    added a PickleDataFolder class
    Former-commit-id: 3277d03dd751950af66ae0f3d67ed391352daf45

--- PickleDataSet.py
+++ PickleDataSet.py
@@ -1,6 +1,33 @@
 from torch.utils.data import get_worker_info
 from torch.utils.data import IterableDataset
 import dicomo
+import os
+
+
+class PickleDataFolder(IterableDataset):
+    def __init__(self, base_path, dicomo_fields, transform=None):
+        assert isinstance(dicomo_fields, list), 'dicomo_fields is not a list'
+        assert isinstance(base_path, str), 'base_path is not a string'
+        self.dicomo_fields = dicomo_fields
+        self.base_path = base_path
+        self.transform = transform
+
+    def __iter__(self):
+        None
+
+    def __next__(self):
+        for data_set in self.get_next_data_set(self):
+            try:
+                while True:
+                    yield next(data_set)
+            except:
+                None
+        raise StopIteration
+
+    def get_next_data_set(self):
+        for root, dirs, files in os.walk(self.base_path):
+            for name in files:
+                yield PickleDataSet(os.path.join(root, name), self.dicomo_fields, self.transform)
 
 
 class PickleDataSet(IterableDataset):
@@ -21,27 +48,29 @@ class PickleDataSet(IterableDataset):
             raise Exception("PickleDataSet does not support multi-process data loading")
 
     def __next__(self):
-        # get next dicomo class from the stream
-        dicomo_class = next(self.pickle_stream)
+        try:
+            # get next dicomo class from the stream
+            dicomo_class = next(self.pickle_stream)
 
-        # fetch values of the fields we are interested in
-        field_list = []
-        for field in self.dicomo_fields:
-            try:
-                # ugly but works
-                # check if transform is specified and if it should be applied
-                temp = getattr(dicomo_class, field)
-                if self.transform and field == 'tensor':
-                    try:
-                        temp = self.transform(temp)
-                    except:
-                        raise Exception('Could not apply specified transformation to the dicom image')
-                field_list.append(temp)
-            except:
-                None
+            # fetch values of the fields we are interested in
+            field_list = []
+            for field in self.dicomo_fields:
+                try:
+                    # ugly but works
+                    # check if transform is specified and if it should be applied
+                    temp = getattr(dicomo_class, field)
+                    if self.transform and field == 'tensor':
+                        try:
+                            temp = self.transform(temp)
+                        except:
+                            raise Exception('Could not apply specified transformation to the dicom image')
+                    field_list.append(temp)
+                except:
+                    None
 
-        return field_list
-
+            return field_list
+        except:
+            raise StopIteration
 
 
 def print_labels_and_display_images(tensors, labels):
