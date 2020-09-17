5690acd8c0db1928dc13a18ebc6e422c488e9308
Niek <n.l.heinen@student.utwente.nl>
2020-09-17 09:43:12 +0200

    wrong import
    Former-commit-id: 9a204fc43f07aa13c48cf1dfdb60db67dd7f7690

--- misc/get_labels.py
+++ misc/get_labels.py
@@ -3,9 +3,9 @@ import sys
 
 # Makes sure we can import dicomo, and run this file from the terminal
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import dicomo
+import classifier
 
-dataloader = dicomo.get_data_loader('/home/nheinen/gemicai/dicom_objects/DX/')
+dataloader = classifier.get_data_loader('/home/nheinen/gemicai/dicom_objects/DX/')
 
 for i, data in enumerate(dataloader, 0):
     # get the inputs; data is a list of [inputs, labels]
