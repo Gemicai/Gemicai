6f899510a641007017ac1432247c16a95fdaf2a2
Niek <n.l.heinen@student.utwente.nl>
2020-09-17 11:11:42 +0200

    Generate bpe labels for all DX files
    Former-commit-id: 28f84415790c45cff8e8126d6a18c74fa1438d23

--- misc/get_labels.py
+++ misc/get_labels.py
@@ -1,35 +1,37 @@
 import os
 import sys
-from collections import Counter
-
 # Makes sure we can import dicomo, and run this file from the terminal
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-import PickleDataSet
 import classifier
 
 data_origin = '/home/nheinen/gemicai/dicom_objects/DX/000001.dicomos.gz'
-data_origin = '../examples/compressed/DX/000001.dicomos.gz'
+# data_origin = '../examples/compressed/DX/000001.dicomos.gz'
 
 
-# pds = PickleDataSet.PickleDataSet(data_origin, ['tensor', 'bpe'])
-# pds_iter = iter(pds)
-# cnt = Counter()
-# s = {}
-# for _ in range(1000):
-#     label = next(pds_iter)[1]
-#     cnt.update(label)
-#     s.update(s)
-#
-# for value, count in cnt.most_common():
-#     print(value, count)
-#
-#
-# # This should work but doesn't
+class Counter:
+    def __init__(self):
+        self.dic = {}
+
+    def update(self, s):
+        if s in self.dic.keys():
+            self.dic[s] += 1
+        else:
+            self.dic[s] = 1
+
+    def print(self):
+        for k, v in self.dic.items():
+            print('{} \t {}'.format(k, v))
+
+
+cnt = Counter()
 dataloader = classifier.get_data_loader(data_directory=data_origin)
 
 for i, data in enumerate(dataloader):
-    # get the inputs; data is a list of [inputs, labels]
-    tensor, labels = data
-    print(data)
+    labels = data[0]
+    for l in labels:
+        cnt.update(l)
+cnt.print()
+print(cnt.dic.keys())
+
 
 
