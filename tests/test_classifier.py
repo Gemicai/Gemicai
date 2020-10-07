916df6c359530f713bf0135ebe3b804e1b8dc331
Mateusz Jaworski <m.jaworski97@wp.pl>
2020-10-08 00:32:53 +0200

    added the rest of test STUBs
    Former-commit-id: 0c7cafa38b27e91942bebea3eafd125b6656a359

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -7,7 +7,111 @@ import unittest
 
 
 class TestClassifier(unittest.TestCase):
-    pass
+
+    def test_init_correct_usage(self):
+        None
+
+    def test_init_wrong_module_type(self):
+        None
+
+    def test_init_wrong_classes_type(self):
+        None
+
+    def test_init_wrong_layer_config_type(self):
+        None
+
+    def test_init_wrong_loss_function_type(self):
+        None
+
+    def test_init_wrong_optimizer_type(self):
+        None
+
+    def test_init_verbosity_2(self):
+        None
+
+    def test_init_wrong_verbosity_type(self):
+        None
+
+    def test_init_wrong_enable_cuda_type(self):
+        None
+
+    def test_init_wrong_cuda_device_type(self):
+        None
+
+    def test_init_wrong_cuda_device_number(self):
+        None
+
+    def test_train_correct_usage(self):
+        None
+
+    def test_train_no_dataset(self):
+        None
+
+    def test_train_wrong_dataset_type(self):
+        None
+
+    def test_train_wrong_batch_size_type(self):
+        None
+
+    def test_train_wrong_epochs_type(self):
+        None
+
+    def test_train_negative_epochs(self):
+        None
+
+    def test_train_wrong_num_workers_type(self):
+        None
+
+    def test_train_negative_num_workers(self):
+        None
+
+    def test_train_pin_memory_wrong_type(self):
+        None
+
+    def test_evaluate_wrong_dataset_type(self):
+        None
+
+    def test_evaluate_wrong_batch_size_type(self):
+        None
+
+    def test_evaluate_wrong_num_workers_type(self):
+        None
+
+    def test_evaluate_def_negative_num_workers(self):
+        None
+
+    def test_evaluate_pin_memory_wrong_type(self):
+        None
+
+    def test_save_correct_path(self):
+        None
+
+    def test_save_invalid_path(self):
+        None
+
+    def test_set_verbosity_2(self):
+        None
+
+    def test_set_verbosity_wrong_type(self):
+        None
+
+    def test_set_device_enable_cuda_wrong_type(self):
+        None
+
+    def test_set_trainable_layers_layers_wrong_type(self):
+        None
+
+    def test_set_trainable_layers_correct_usage(self):
+        None
+
+    def test_from_file_invalid_path(self):
+        None
+
+    def test_from_file_wrong_file_format(self):
+        None
+
+    def test_from_file_correct_usage(self):
+        None
 
 
 if __name__ == '__main__':
--- tests/test_data_iterators.py
+++ tests/test_data_iterators.py
@@ -7,23 +7,198 @@ import unittest
 
 
 class TestDicomoDataset(unittest.TestCase):
-    pass
+
+    def test_from_file_correct_usage(self):
+        None
+
+    def test_from_file_wrong_file_path(self):
+        None
+
+    def test_from_file_wrong_file_type(self):
+        None
+
+    def test_from_file_wrong_label_type(self):
+        None
+
+    def test_from_file_no_labels(self):
+        None
+
+    def test_from_file_test_label_existence(self):
+        None
+
+    def test_from_file_constraints_wrong_type(self):
+        None
+
+    def test_from_file_constraints_empty(self):
+        None
+
+    def test_from_file_check_contraint(self):
+        None
+
+    def test_from_file_apply_invalid_transformation(self):
+        None
+
+    def test_from_file_apply_valid_transformation(self):
+        None
+
+    def test_from_file_correct_usage(self):
+        None
+
+    def test_from_directory_file_wrong_directory_path(self):
+        None
+
+    def test_from_directory_file_wrong_label_type(self):
+        None
+
+    def test_from_directory_file_no_labels(self):
+        None
+
+    def test_from_directory_file_test_label_existence(self):
+        None
+
+    def test_from_directory_file_constraints_wrong_type(self):
+        None
+
+    def test_from_directory_file_constraints_empty(self):
+        None
+
+    def test_from_directory_file_check_contraint(self):
+        None
+
+    def test_from_directory_file_apply_invalid_transformation(self):
+        None
+
+    def test_from_directory_file_apply_valid_transformation(self):
+        None
+
+    def test_get_dicomo_dataset_correct_usage_file(self):
+        None
+
+    def test_get_dicomo_dataset_correct_usage_directory(self):
+        None
 
 
 class TestPickledDicomoDataSet(unittest.TestCase):
-    pass
 
+    def test_init_correct_usage(self):
+        None
 
-class TestPickledDicomoDataFolder(unittest.TestCase):
-    pass
+    def test_init_invalid_file_path(self):
+        None
+
+    def test_init_file_has_wrong_type(self):
+        None
+
+    def test_init_wrong_labels_type(self):
+        None
+
+    def test_init_wrong_constraints_type(self):
+        None
+
+    def test_iter(self):
+        None
+
+    def test_next(self):
+        None
+
+    def test_len(self):
+        None
+
+    def test_from_file_apply_invalid_transformation(self):
+        None
+
+    def test_from_file_apply_valid_transformation(self):
+        None
+
+    def test_subset_correct_usage(self):
+        None
+
+    def test_subset_wrong_constraint_type(self):
+        None
 
 
 class TestPickledDicomoFilePool(unittest.TestCase):
-    pass
+
+    def test_init_correct_usage(self):
+        None
+
+    def test_init_invalid_file_pool_type(self):
+        None
+
+    def test_init_invalid_file_pool_path(self):
+        None
+
+    def test_init_file_has_wrong_type(self):
+        None
+
+    def test_init_wrong_labels_type(self):
+        None
+
+    def test_init_wrong_constraints_type(self):
+        None
+
+    def test_iter(self):
+        None
+
+    def test_next(self):
+        None
+
+    def test_len(self):
+        None
 
 
-class TestConcurrentPickledDicomoTaskSplitter(unittest.TestCase):
-    pass
+class TestPickledDicomoDataFolder(unittest.TestCase):
+
+    def test_init_correct_usage(self):
+        None
+
+    def test_init_invalid_directory_path(self):
+        None
+
+    def test_init_file_has_wrong_type(self):
+        None
+
+    def test_init_wrong_labels_type(self):
+        None
+
+    def test_init_wrong_constraints_type(self):
+        None
+
+    def test_iter(self):
+        None
+
+    def test_next(self):
+        None
+
+    def test_len(self):
+        None
+
+
+class TestConcurrentPickledDicomObjectTaskSplitter(unittest.TestCase):
+
+    def test_init_correct_usage(self):
+        None
+
+    def test_init_invalid_directory_path(self):
+        None
+
+    def test_init_wrong_labels_type(self):
+        None
+
+    def test_init_wrong_constraints_type(self):
+        None
+
+    def test_iter(self):
+        None
+
+    def test_next(self):
+        None
+
+    def test_len(self):
+        None
+
+    def test_an_be_parallelized(self):
+        None
 
 
 if __name__ == '__main__':
