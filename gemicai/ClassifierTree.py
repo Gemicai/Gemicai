8386bfb223ff233120e6e63d2403829ad69e9ef0
Niek Heinen <niekheinen@gmail.com>
2020-10-10 18:58:50 +0200

    WIP: ClassifierTree refactor
    Former-commit-id: bfb1a3be40bf6f199109b04568cb5e94aaed2262

--- gemicai/ClassifierTree.py
+++ gemicai/ClassifierTree.py
@@ -5,21 +5,20 @@ from tabulate import tabulate
 
 
 class ClassifierTree:
-    def __init__(self, default_classifier, labels, base_dataset, dataset_constraints={}, verbosity=0):
-        if not isinstance(default_classifier, gem.Classifier):
-            raise Exception('default classifier should be a gem.Classifier')
+    def __init__(self, labels, default_classifier=None, base_dataset=None, dataset_constraints={}, verbosity=0):
         if not isinstance(labels, list):
             raise Exception('classify should be a list of attributes the tree should classify')
         assert len(labels) >= 2, 'labels should contain at least 2 labels.'
         if not isinstance(base_dataset, gem.GemicaiDataset):
             raise Exception('base_dataset should be an instance of GemicaiDataset')
+        if not isinstance(default_classifier, gem.Classifier):
+            raise Exception('default classifier should be a gem.Classifier')
 
         # Set root of the tree
         classes = base_dataset.subset(dataset_constraints).classes(labels[0])
         self.root = ClassifierNode(default_classifier, labels[0], classes, dataset_constraints)
 
         # Children can either be a ClassifierNode or ClassifierTree
-        self.children = {}
         self.labels = labels
 
         # Recursively sets up the ClassifierTree
@@ -31,9 +30,9 @@ class ClassifierTree:
             # Else not the end of the tree, which means this child must be an instance of ClassifierTree.
             if len(labels) == 2:
                 child_classes = base_dataset.subset(cons).classes(labels[1])
-                self.children[c] = ClassifierNode(default_classifier, labels[1], child_classes)
+                child = ClassifierNode(default_classifier, labels[1], child_classes)
             else:
-                self.children[c] = ClassifierTree(default_classifier, labels[1:], base_dataset, cons)
+                child = ClassifierTree(default_classifier, labels[1:], base_dataset, cons)
 
     def __str__(self):
         data, dic = [], {}
@@ -67,24 +66,15 @@ class ClassifierTree:
                 s.append(c)
         return s
 
-    # save classifier object to .pkl file, can be retrieved with load_classifier()
-    def save(self, file_path=None):
-        if not isinstance(file_path, str):
-            raise TypeError("save method expects a file_path to be an instance of string")
-        with open(file_path, 'wb') as output:
-            pickle.dump(self, output, pickle.HIGHEST_PROTOCOL)
-
-    # Loads ClassifierTree object from .pkl file
+    # Instantiates ClassifierTree object from ClassifierNode, or ClassifierNode filepath
     @staticmethod
-    def from_file(pkl_file_path=None):
-        if not isinstance(pkl_file_path, str):
-            raise TypeError("load_from_pickle method expects a pkl_file_path to be an instance of string")
+    def from_node(node=None):
+        if isinstance(node, str):
+            with open(node, 'rb') as inp:
+                node = pickle.load(inp)
+        if not isinstance(node, ClassifierNode):
+            raise TypeError('Not a valid ClassifierNode object')
 
-        with open(pkl_file_path, 'rb') as inp:
-            tree = pickle.load(inp)
-            if not isinstance(tree, ClassifierTree):
-                raise TypeError(pkl_file_path + ' does not contain a valid ClassifierTree class object')
-            return tree
 
 
 # label: this is the label the node classifies e.g. 'BodyPartExamined'
@@ -100,6 +90,10 @@ class ClassifierNode:
         self.label = label
         self.dataset_constraints = dataset_constraints
 
+        # ClassifierNode will contain meta data about the classifier like how accurate it is. Otherwise training and
+        # evaluating a big ClassifierTree is going to be impossible.
+        self.accuracy = '77.77%'
+
     def train(self, dataset, epochs=20, num_workers=0, pin_memory=False, verbosity=0):
         dataset = dataset.subset(self.dataset_constraints)
         return self.classifier.train(dataset, epochs=epochs, num_workers=num_workers, pin_memory=pin_memory,
@@ -108,3 +102,17 @@ class ClassifierNode:
     def evaluate(self, dataset, num_workers=0, pin_memory=False, verbosity=0):
         dataset = dataset.subset(self.dataset_constraints)
         return self.classifier.evaluate(dataset, num_workers=num_workers, pin_memory=pin_memory, verbosity=verbosity)
+
+    # save ClassifierNode object to .gemnode file, can be retrieved with from_file()
+    def save(self, file_path):
+        with open(file_path, 'wb') as out:
+            pickle.dump(self, out, pickle.HIGHEST_PROTOCOL)
+
+    # Loads classifier object from .gemnode file
+    @staticmethod
+    def from_file(file_path):
+        with open(file_path, 'rb') as inp:
+            gemnode = pickle.load(inp)
+            if not isinstance(gemnode, ClassifierNode):
+                raise TypeError(file_path + ' does not contain a valid ClassifierNode object')
+            return gemnode
