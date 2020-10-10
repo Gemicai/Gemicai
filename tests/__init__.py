ad19d595f0f61a7c2f3b6b49bdf380ab0b6e61a3
Niek <n.l.heinen@student.utwente.nl>
2020-10-10 16:27:56 +0200

    Cleaner import in test files with an __init__.py, not yet working tho

---
+++ tests/__init__.py
@@ -0,0 +1,11 @@
+
+
+import inspect
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir)
+
+from .. import gemicai
