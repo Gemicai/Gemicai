d1514d2ee9905992c38944ee2ff0a300fc4754c3
Niek <n.l.heinen@student.utwente.nl>
2020-10-15 15:56:56 +0200

    Added IO module, will implement that sometime soon. Also added trees for testing.
    Former-commit-id: f04c77c943b766174bc63c54a1dc1f1ec5b12bc0

---
+++ gemicai/io/pickle.py
@@ -0,0 +1,4 @@
+# All IO should be handled here, also easier to refactor code when we or someone else in the future decides that theres
+# something better than pickle to write .gemset and .gemnode
+
+import pickle
