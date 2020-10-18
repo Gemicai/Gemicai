e8764243fac7bb3bb22dab6a49e05662ef4d2b41
Niek <niekheinen@gmail.com>
2020-10-18 23:22:49 +0200

    Finishing touches, to the tutorial, ready for progress presentation wednesday
    Former-commit-id: 052309d52601a358f3570316bb3bdc28d4991fec

--- gemicai/data_inspection.py
+++ gemicai/data_inspection.py
@@ -1,16 +1,19 @@
 # Everything concerning inspecting data
 import gemicai as gem
-import matplotlib as plt
+from matplotlib import pyplot as plt
+import torch
 
 
+# TODO: Implement so that this function actually writes back to gemset
 def correct_dataset(net: gem.Classifier, dataset: gem.GemicaiDataset):
     for tensor, label in dataset:
         prediction = net.classify(tensor)
         if prediction[0][0] != label:
             print('True label: {}\nPrediction : {}'.format(label, prediction))
-            plt.imshow(tensor)
+            plt.imshow(torch.transpose(tensor, 0, 2))
             plt.show()
             input('Overwrite class?')
+            print('\n\n')
 
 
 
